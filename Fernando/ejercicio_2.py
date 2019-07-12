a1260a954c4062185d9087201c1787e986639a8c
Fernando Agostino <fernando.agostino@vivaltotech.com>
2019-07-11 22:55:10 -0300

    Corregido para sumar solo los pares

--- Fernando/ejercicio_2.py
+++ Fernando/ejercicio_2.py
@@ -17,11 +17,13 @@ a, b, suma = 1, 2, 0
 
 # Empiezo un lazo que terminará al superar el valor límite
 while a <= limite:
-    if imprimir:
-        # En vez de agregar \n al final, termino la línea con espacio
-        print(a, end=" ")
-    # Acumulo el valor del término actual
-    suma += a
+    # Me fijo si el término es par
+    if a % 2 == 0:
+        # Acumulo el valor del término actual sólo si es par
+        suma += a
+        if imprimir:
+            # En vez de agregar \n al final, termino la línea con espacio
+            print(a, end=" ")
     # Calculo el nuevo término y reasigno las variables
     a, b = b, a + b
 
